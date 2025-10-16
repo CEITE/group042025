01150a4c4a722cdbedca475389142875a85acf81
airaalimorom <airaalimorom89@gmail.com>
2025-10-16 13:19:44 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,10 +1,10 @@
 <?php
-$hostname = "localhost"; 
+$servername = "localhost"; 
 $username = "u765170597_group042025"; 
 $password = "He=;?Ke2/s"; 
-$dbname = "u765170597_group042025"; 
+$database = "u765170597_group042025"; 
 
-$conn = new mysqli($hostname,$username,$password,$dbname);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
