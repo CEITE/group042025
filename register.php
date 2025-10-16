1a0141214236fbcc6d8f6bf2872039f35f2196fb
airaalimorom <airaalimorom89@gmail.com>
2025-10-16 13:06:03 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,7 @@
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 <?php
 session_start();
 include("conn.php");
@@ -309,3 +313,4 @@ if (isset($_POST['resend_otp'])) {
     }
 }
 ?>
+
