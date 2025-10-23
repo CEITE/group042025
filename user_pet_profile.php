eccc7a05835c9c7ad1e9ff789e1f9579fd0d4c18
airaalimorom <airaalimorom89@gmail.com>
2025-10-24 00:37:36 +0800

    Update user_pet_profile.php

--- user_pet_profile.php
+++ user_pet_profile.php
@@ -360,7 +360,7 @@ $pets = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
         <a href="register_pet.php">
             <div class="icon"><i class="fa-solid fa-plus-circle"></i></div> Register Pet
         </a>
-        <a href="#">
+        <a href="user_setting.php">
             <div class="icon"><i class="fa-solid fa-gear"></i></div> Settings
         </a>
         <a href="logout.php" class="logout">
@@ -674,5 +674,6 @@ $pets = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 </script>
 </body>
 </html>
+
 
 
