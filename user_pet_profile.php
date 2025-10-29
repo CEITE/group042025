2eb97fd279ceefa3b1d14fad87e231e916b98698
airaalimorom <airaalimorom89@gmail.com>
2025-10-29 20:59:08 +0800

    Update user_pet_profile.php

--- user_pet_profile.php
+++ user_pet_profile.php
@@ -130,7 +130,6 @@ if (isset($_POST['delete_pet'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
-      <style>
     :root {
         --primary: #0ea5e9;
         --primary-dark: #0284c7;
@@ -578,7 +577,6 @@ if (isset($_POST['delete_pet'])) {
             grid-template-columns: 1fr;
         }
     }
-</style>
     </style>
 </head>
 <body>
@@ -1164,4 +1162,5 @@ if (isset($_POST['delete_pet'])) {
 </script>
 </body>
 </html>
+
 
