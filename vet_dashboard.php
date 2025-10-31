5643cca66a5d4b197b0251f74b3891c51586f0ae
airaalimorom <airaalimorom89@gmail.com>
2025-10-31 17:25:13 +0800

    Update vet_dashboard.php

--- vet_dashboard.php
+++ vet_dashboard.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'vet') {
     header("Location: login_vet.php");
     exit();
 }
-
+$user_id = $_SESSION['user_id'];
 
 // Handle profile picture upload
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['profile_picture'])) {
@@ -1390,6 +1390,7 @@ foreach ($missing_notifications as $appointment) {
 </script>
 </body>
 </html>
+
 
 
 
