9a10acf657170a4f025eed2fb6fc0bfaf23d5f50
airaalimorom <airaalimorom89@gmail.com>
2025-10-31 22:22:33 +0800

    Update user_dashboard.php

--- user_dashboard.php
+++ user_dashboard.php
@@ -300,7 +300,7 @@ if (isset($_POST['cancel_appointment'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VetCareQR - User Dashboard</title>
+    <title>VetCareQR - BrightView Veterinary Clinic</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -1600,6 +1600,7 @@ if (isset($_POST['cancel_appointment'])) {
 </script>
 </body>
 </html>
+
 
 
 
